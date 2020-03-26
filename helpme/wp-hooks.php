11416b2ed7bffa6a322f3f3f693a90434979b600
makiomar <mo7amed.maki@gmail.com>
2020-03-26 10:35:53 +0300

    add mail helper hooks

--- helpme/wp-hooks.php
+++ helpme/wp-hooks.php
@@ -112,3 +112,35 @@ add_action( 'wp_print_footer_scripts', function(){
 	
 
 <?php });
+
+/**
+ * Filter the mail content type.
+ */
+add_filter( 'wp_mail_content_type', function() {
+    return 'text/html';
+} );
+
+//set page direction in email template
+add_filter(
+	'wp_mail',
+	function ($args){
+
+
+	    if(function_exists('is_rtl') && is_rtl()){
+	      $dir   = 'rtl';
+	      $align = 'right';
+	    }else{
+	      $dir   = 'ltr';
+	      $align = 'left';
+	    }
+
+		$message = sprintf(
+					'<div dir="%1$s" style="text-align:%2$s">'. $args['message']. '</div>', 
+					$dir, 
+					$align
+				);
+		$args['message'] = $message;
+		
+		return $args;
+	}
+, 99,1);

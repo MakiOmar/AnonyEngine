5bf42734b3191771a863aa6defde0b62a0340e2d
makiomar <mo7amed.maki@gmail.com>
2020-03-28 06:58:24 +0300

    Make separate view file for checkbox

--- input-fields/fields/checkbox/checkbox.php
+++ input-fields/fields/checkbox/checkbox.php
@@ -19,71 +19,41 @@ class ANONY_Checkbox
 		if (!is_object($parent)) return;
 
 		$this->parent = $parent;
+
+		$this->data = [
+
+			'value'   => $this->parent->value ? $this->parent->value : 0, //0 is fix for value "off = 0"
+
+		    'note'    => isset($this->parent->field['note']) ? esc_html($this->parent->field['note']) : false,
+
+			'id'      => esc_attr($this->parent->field['id']),
+
+			'context' => $this->parent->context,
+
+			'title'   => isset($this->parent->field['title']) ? esc_html($this->parent->field['title']) : false,
+
+			'name'    => esc_attr( $this->parent->input_name ),
+
+			'options' => isset($this->parent->field['options'] ) ? $this->parent->field['options'] : false,
+
+			'class'   => esc_attr( $this->parent->class_attr),
+
+			'desc'    => isset($this->parent->field['desc']) ? esc_html($this->parent->field['desc']) : false,
+
+			'disabled' => isset( $this->parent->field['disabled'] ) && ( $this->parent->field['disabled'] == true ) ? " disabled = 'disabled' " : "",
+		];
+
 	}
 
-	public function render(){	
+	public function render(){
 
-		$disabled = isset( $this->parent->field['disabled'] ) && ( $this->parent->field['disabled'] == true ) ? " disabled = 'disabled' " : "";
+		ob_start();
 
-		// fix for value "off = 0"
-		if( ! $this->parent->value ) $this->parent->value = 0;
+		include 'checkbox.view.php';
 
-		if(isset($this->parent->field['note'])){
-			echo '<p class=anony-warning>'.$field['note'].'<p>';
-		}
+		$html = ob_get_contents();
 
-		$html	= sprintf( 
-						'<fieldset class="anony-row anony-row-inline" id="fieldset_%1$s">', 
-						$this->parent->field['id'] 
-					);
-		if($this->parent->context == 'meta' && isset($this->parent->field['title'])){
-			$html   .= sprintf(
-							'<label class="anony-label" for="%1$s">%2$s</label>', 
-							$this->parent->field['id'],
-							$this->parent->field['title']
-						  ) ;
-		}
-
-		// fix for WordPress 3.6 meta options
-		if(strpos( $this->parent->field['id'] ,'[]') === false) 
-
-			$html .= sprintf(
-						'<input type="hidden" name="%1$s" value="0" />', 
-						$this->parent->input_name
-					);
-		$html .= '<div class="anony-metabox-col">';
-
-		if(isset($this->parent->field['options']) && is_array($this->parent->field['options'])){
-			foreach($this->parent->field['options'] as $opt => $title){
-
-				$checked = (is_array($this->parent->value[0]) && in_array($opt, $this->parent->value[0])) ? ' checked="checked"' : '';
-
-				$html   .= sprintf(
-							'<label class="anony-inputs-row">%1$s', 
-							$title
-						  ) ;
-
-				$html   .= sprintf('<input type="checkbox" class="checkbox %1$s" id="%2$s[%3$s]" name="%4$s[]" value="%3$s"%5$s%6$s/></label>',
-							$this->parent->class_attr,
-							$this->parent->field['id'],
-							$opt, 
-							$this->parent->input_name,  
-							$checked,
-							$disabled
-						  ) ;
-			}
-
-		$html .= '</div>';
-
-		}else{
-
-			$html .= sprintf('<input type="checkbox" id="%1$s" name="%2$s" class="checkbox %3$s" value="1"%4$s%5$s/>',$this->parent->field['id'], $this->parent->input_name, $this->parent->class_attr, checked($this->parent->value, 1, false), $disabled);
-		}
-		
-		
-		$html .= (isset($this->parent->field['desc']) && !empty($this->parent->field['desc'])) ? '&nbsp;&nbsp;<div class="description btn-desc">'.$this->parent->field['desc'].'</div>':'';
-
-		$html	.= '</fieldset>';
+		ob_end_clean();
 
 		return $html;
 	}
---
+++ input-fields/fields/checkbox/checkbox.view.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Checkbox view
+ *
+ * @package Anonymous theme
+ * @author Makiomar
+ * @link http://makiomar.com
+ */
+extract($this->data);
+
+if($note):?>
+<!--@if ($note === true)--> 
+
+<p class=anony-warning><?= $note ?><p>
+<!--<p class=anony-warning>{{ $note }}<p>-->
+
+<?php endif ?>
+<!--@endif-->
+
+<fieldset class="anony-row anony-row-inline" id="fieldset_<?= $id?>">
+<!--<fieldset class="anony-row anony-row-inline" id="fieldset_{{ $id }}">-->
+				 
+	<?php if($context == 'meta' && $title ) :?>
+	<!--@if ($context == 'meta' && $title)-->
+
+		<label class="anony-label" for="<?= $id ?>"><?= $title ?></label>
+
+	<?php endif;
+
+	// fix for WordPress 3.6 meta options
+	if(strpos( $id ,'[]') === false) :?>
+	<!--@if (strpos( $id ,'[]') === false)-->
+
+		<input type="hidden" name="<?= $name?>" value="0"/>
+
+	<?php endif?> 
+					
+	<div class="anony-metabox-col">
+
+	<?php if($options && is_array($options)):
+
+		foreach($options as $opt => $title):
+		//@foreach ($options as $opt = $title)
+
+			$checked = (is_array($value) && in_array($opt, $value)) ? ' checked="checked"' : '';?>
+
+			<label class="anony-inputs-row"> <?= $title ?> 
+						
+				<input type="checkbox" class="checkbox <?= $class ?>" id="<?= $id ?>[<?= $opt ?>]" name="<?= $name ?>[]" value="<?= $opt ?>"<?= $checked ?><?= $disabled ?>/>
+
+			</label>
+
+		<?php endforeach ?>
+
+	</div>
+
+	<?php else: $checked = checked($value, 1, false);?>
+
+		<input type="checkbox" id="<?= $id ?>" name="<?= $$name ?>" class="checkbox <?= $class ?>" value="1"<?= $checked ?><?= $disabled ?>/>
+
+	<?php endif;
+
+	if($desc && !empty($desc)):?>
+
+		&nbsp;&nbsp;<div class="description btn-desc"><?= $desc?></div>
+
+	<?php endif ?>
+
+
+</fieldset>

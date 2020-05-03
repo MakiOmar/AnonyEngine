0ae6323e05fd1c758dce8d1c607bf061f2942588
makiomar <mo7amed.maki@gmail.com>
2020-05-03 19:46:34 +0300

    add jquery helpers

--- assets/js/jquery.helpme.js
+++ assets/js/jquery.helpme.js
@@ -33,5 +33,42 @@ jQuery(document).ready(function($){
 	$.fn.AnonyDateTimePicker = function(fieldId, getWhat, DateTimeOptions){
 	    $(fieldId)[getWhat](DateTimeOptions);
 	};
+	
+	
+	/**
+	 * This is then function used to detect if the element is scrolled into view
+	 *
+	 * @param object elem Jquery object of an element
+	 */
+	$.fn.elementScrolled = function(elem){
+
+		var docViewTop = $(window).scrollTop();
+
+		var docViewBottom = docViewTop + $(window).height();
+
+		var elemTop = elem.offset().top;
+
+		return ((elemTop <= docViewBottom) && (elemTop >= docViewTop));
+
+	}
+	/**
+	 * Slide element to top if been into view.
+	 *
+	 * @param string     element Element selector
+	 * @param slideClass Reposition class
+	 */
+	$.fn.slideToTop = function (element, slideClass){
+		$(element).each(function(){
+
+			if($.fn.elementScrolled($(this))) {
+
+			  if(!$(this).hasClass(slideClass)){
+
+				  $(this).addClass(slideClass);
+
+			  }
+			}
+		});
+	}
 });
 

f02f1256fea1679833b09277bfeb999b0c1b5396
makiomar <mo7amed.maki@gmail.com>
2020-10-12 18:07:35 +0200

    Add helper to activate plugin

--- helpme/wp/wpplugin-help.php
+++ helpme/wp/wpplugin-help.php
@@ -25,5 +25,26 @@ if ( ! class_exists( 'ANONY_WPPLUGIN_HELP' ) ) {
 			return false;
 		}
 		
+		/**
+		 * Activate plugin
+		 *
+		 * @var string $path  Path of plugin file (e.g akismet/akismet.php)
+		 */
+		static function activatePlugin( $plugin ) {
+		    $current = get_option( 'active_plugins' );
+		    $plugin = plugin_basename( trim( $plugin ) );
+
+		    if ( !in_array( $plugin, $current ) ) {
+		        $current[] = $plugin;
+		        sort( $current );
+		        do_action( 'activate_plugin', trim( $plugin ) );
+		        update_option( 'active_plugins', $current );
+		        do_action( 'activate_' . trim( $plugin ) );
+		        do_action( 'activated_plugin', trim( $plugin) );
+		    }
+
+		    return null;
+		}
+		
 	}
 }

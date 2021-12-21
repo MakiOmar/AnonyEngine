2d6a6240fd3e792ab52cc44a4bb84cb237ca279f
unknown <mo7amed.maki@gmail.com>
2021-12-21 20:55:20 +0200

    Add cookies helpers

--- assets/js/jquery.helpme.js
+++ assets/js/jquery.helpme.js
@@ -1,6 +1,29 @@
 jQuery(document).ready(function($){
 	'use strict';
-
+	$.fn.AnonyCreateCookie = function(name,value,minutes) {
+        if (minutes) {
+            var date = new Date();
+            date.setTime(date.getTime()+(minutes*60*1000));
+            var expires = "; expires="+date.toGMTString();
+        } else {
+            var expires = "";
+        }
+        document.cookie = name+"="+value+expires+"; path=/";
+    }
+    $.fn.AnonyReadCookie = function(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+           if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    
+        }
+        return null;
+    }
+    $.fn.AnonyEraseCookie = function(name) {
+	    createCookie(name,"",-1);
+	}​
 	//Apply mutation observer on a querySelector and apply a callback function
 	$.fn.AnonyObserve = function(querySelector, callback){
 

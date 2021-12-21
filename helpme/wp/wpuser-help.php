74757901899053438977ab4e7fadd875a267c514
unknown <mo7amed.maki@gmail.com>
2021-12-21 20:57:49 +0200

    Helper to create member account number

--- helpme/wp/wpuser-help.php
+++ helpme/wp/wpuser-help.php
@@ -26,6 +26,36 @@ if ( ! class_exists( 'ANONY_WPUSER_HELP' ) ) {
 			}
 
 			return false;
-		 }
+		}
+		
+		/**
+		 * Generate member_number when a user is registered.
+		 * Should be hooked to user_register
+		 */ 
+		static function generateMemberNumber($user_id)
+		{
+			$member_number = get_user_meta($user_id, "member_number", true);
+			
+			//if no member number, create one
+			if(empty($member_number))
+			{
+				global $wpdb;
+				
+				//this code generates a string 10 characters long of numbers and letters
+				while(empty($member_number))
+				{
+					$scramble = md5(AUTH_KEY . current_time('timestamp') . $user_id . SECURE_AUTH_KEY);
+					$member_number = substr($scramble, 0, 10);
+					$check = $wpdb->get_var("SELECT meta_value FROM $wpdb->usermeta WHERE meta_value = '" . esc_sql($member_number) . "' LIMIT 1");
+					if($check || is_numeric($member_number))
+						$member_number = NULL;
+				}
+				
+				//save to user meta
+				update_user_meta($user_id, "member_number", $member_number);
+				
+				return $member_number;
+			}
+		}
 	}
 }

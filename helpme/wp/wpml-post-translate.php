46d3ffa6539615c0026ba882c6d8c25b7ddacf8b
unknown <mo7amed.maki@gmail.com>
2022-02-05 09:14:13 +0200

    Add helper file to handle post translation

---
+++ helpme/wp/wpml-post-translate.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * WPML Post helpers class
+ *
+ * @package Anonymous theme
+ * @author Makiomar
+ * @link http://makiomar.com
+ */
+
+if ( ! class_exists( 'ANONY_WPML_POST_TRANSLATE' ) ) {
+	class ANONY_WPML_POST_TRANSLATE extends ANONY_WPML_HELP{
+		
+		
+		
+	}
+}

125923e78ca1ef28ac1788a7b562f93e41c21bf4
unknown <mo7amed.maki@gmail.com>
2022-03-03 10:08:52 +0200

    Add helper to group top terms with their children

--- helpme/wp/term-help.php
+++ helpme/wp/term-help.php
@@ -8,32 +8,34 @@
  */
 
 if ( ! class_exists( 'ANONY_TERM_HELP' ) ) {
-	class ANONY_TERM_HELP extends ANONY_HELP{
+	class ANONY_TERM_HELP extends ANONY_HELP {
 
 		/**
 		 * Gets an array of human readable terms slug names by taxonomy.
-		 * 
+		 *
 		 * Use instead of get_terms for admin purpuses.
-		 * @param  string  $taxonomy Taxonomy to get terms from
+		 *
+		 * @param  string $taxonomy Taxonomy to get terms from
 		 * @return arrsy             An indexed array of terms slugs
 		 */
 
-		static function queryTermsinAdmin($taxonomy){
-			
+		static function queryTermsinAdmin( $taxonomy ) {
+
 			$terms = ANONY_ARRAY_HELP::ObjToAssoc(
-						ANONY_TERM_HELP::queryTermsByTaxonomy($taxonomy),
-						'', 
-						'slug'
-					);
-			return array_map('urldecode', $terms);
+				self::queryTermsByTaxonomy( $taxonomy ),
+				'',
+				'slug'
+			);
+			return array_map( 'urldecode', $terms );
 		}
 
 		/**
 		 * Query terms slug names by taxonomy
-		 * @param  string  $taxonomy taxonomy to get terms from
+		 *
+		 * @param  string $taxonomy taxonomy to get terms from
 		 * @return array             An array of terms objects contains only slug name
 		 */
-		static function queryTermsSlugsByTaxonomy($taxonomy){
+		static function queryTermsSlugsByTaxonomy( $taxonomy ) {
 			global $wpdb;
 			$query = "SELECT DISTINCT 
 						t.slug 
@@ -46,19 +48,20 @@ if ( ! class_exists( 'ANONY_TERM_HELP' ) ) {
 						WHERE 
 							tax.taxonomy = '$taxonomy'";
 
-			$result =  $wpdb->get_results($query);
+			$result = $wpdb->get_results( $query );
 
-			ANONY_WPDEBUG_HELP::printDbErrors($result);
+			ANONY_WPDEBUG_HELP::printDbErrors( $result );
 
-			return $result;                 
+			return $result;
 		}
 
 		/**
 		 * Query terms by taxonomy
-		 * @param  string  $taxonomy taxonomy to get terms from
+		 *
+		 * @param  string $taxonomy taxonomy to get terms from
 		 * @return array             An array of terms objects
 		 */
-		static function queryTermsByTaxonomy($taxonomy, $operator = '='){
+		static function queryTermsByTaxonomy( $taxonomy, $operator = '=' ) {
 			global $wpdb;
 			$query = "SELECT 
 							* 
@@ -71,66 +74,69 @@ if ( ! class_exists( 'ANONY_TERM_HELP' ) ) {
 						WHERE 
 							tax.taxonomy $operator '$taxonomy'";
 
-			$result =  $wpdb->get_results($query);
+			$result = $wpdb->get_results( $query );
 
-			ANONY_WPDEBUG_HELP::printDbErrors($result);
+			ANONY_WPDEBUG_HELP::printDbErrors( $result );
 
-			return $result;                 
+			return $result;
 		}
 
 		/**
 		 * Get terms using WP_Term_Query class.
-		 * 
+		 *
 		 * Use instead of get_terms for admin purpuses.
-		 * @param  string  $tax    Taxonomy to get terms from
-		 * @param  string  $fields Fields to fetch.
+		 *
+		 * @param  string $tax    Taxonomy to get terms from
+		 * @param  string $fields Fields to fetch.
 		 * @return array             array of terms (id, name, slug)
 		 */
 
-		static function wpTermQuery($tax, $fields){
+		static function wpTermQuery( $tax, $fields ) {
 			/**
 			 * 'fields' to return Accepts:
 			 * 'all' (returns an array of complete term objects),
-			 * 'all_with_object_id' (returns an array of term objects with the 'object_id' param; works only when 
-			 * the $object_ids parameter is populated), 
-			 * 'ids' (returns an array of ids), 
-			 * 'tt_ids' (returns an array of term taxonomy ids), 
-			 * 'id=>parent' (returns an associative array with ids as keys, parent term IDs as values), 
-			 * 'names' (returns an array of term names), 
-			 * 'count' (returns the number of matching terms), 
-			 * 'id=>name' (returns an associative array with ids as keys, term names as values), or 
+			 * 'all_with_object_id' (returns an array of term objects with the 'object_id' param; works only when
+			 * the $object_ids parameter is populated),
+			 * 'ids' (returns an array of ids),
+			 * 'tt_ids' (returns an array of term taxonomy ids),
+			 * 'id=>parent' (returns an associative array with ids as keys, parent term IDs as values),
+			 * 'names' (returns an array of term names),
+			 * 'count' (returns the number of matching terms),
+			 * 'id=>name' (returns an associative array with ids as keys, term names as values), or
 			 * 'id=>slug' (returns an associative array with ids as keys, term slugs as values)
 			 */
 			$termsObject = new WP_Term_Query(
-									array(
-										'taxonomy' => $tax,
-										'fields'   => $fields
-									)
-								);
-				
-			if(!empty ($termsObject->terms)) return $termsObject->terms;
+				array(
+					'taxonomy' => $tax,
+					'fields'   => $fields,
+				)
+			);
+
+			if ( ! empty( $termsObject->terms ) ) {
+				return $termsObject->terms;
+			}
 
 			return '';
 		}
 		/**
 		 * Gets post terms from child up to first parent
-		 * 
+		 *
 		 * @param  int  $id   Term id
 		 * @param  type $tax  Term taxonomy
-		 * @return string     Dash separated terms IDs 
+		 * @return string     Dash separated terms IDs
 		 */
 		static function termParents( $id, $tax ) {
 			$terms  = '';
 			$parent = get_term( $id, $tax );
 
-			if ( is_wp_error( $parent ) )
-				{return '';}
+			if ( is_wp_error( $parent ) ) {
+				return '';}
 
-			$terms .= $parent->term_id ;
+			$terms .= $parent->term_id;
 
 			if ( $parent->parent && ( $parent->parent != $parent->term_id ) ) {
 
-				$terms .= '-'.ANONY_TERM_HELP::termParents( $parent->parent, $tax );
+				$terms .= '-' . self::termParents( $parent->parent, $tax );
 
 			}
 			return $terms;
@@ -138,105 +144,139 @@ if ( ! class_exists( 'ANONY_TERM_HELP' ) ) {
 
 		/**
 		 * Delete all terms connected supplied taxonomies. Can also delete taxonomy
-		 * 
+		 *
 		 * @param array $taxonomies Array of taxonomies to delete terms connected to.
 		 * @param bool  $dlt_tax    Boolean to decide weather to delete a taxonomy. default false
-		 *
 		 */
-		static function deleteTerms($taxonomies, $dlt_tax = false){
+		static function deleteTerms( $taxonomies, $dlt_tax = false ) {
 			global $wpdb;
 			foreach ( $taxonomies as $taxonomy ) {
 				// Prepare & excecute SQL, Delete Terms
 				$result = $wpdb->get_results( $wpdb->prepare( "DELETE t.*, tt.* FROM $wpdb->terms AS t INNER JOIN $wpdb->term_taxonomy AS tt ON t.term_id = tt.term_id WHERE tt.taxonomy IN ('%s')", $taxonomy ) );
 				// Delete Taxonomy
-				if($dlt_tax) $wpdb->delete( $wpdb->term_taxonomy, array( 'taxonomy' => $taxonomy ), array( '%s' ) );
+				if ( $dlt_tax ) {
+					$wpdb->delete( $wpdb->term_taxonomy, array( 'taxonomy' => $taxonomy ), array( '%s' ) );
+				}
 			}
 		}
-		
+
 		/**
 		 * Get term by id
-		 * @param int    $term_id 
-		 * @param string $taxonomy 
+		 *
+		 * @param int    $term_id
+		 * @param string $taxonomy
 		 * @return mixed
 		 */
-		static function getTermBy($term_id, $taxonomy){
-			
-			if(ANONY_WPML_HELP::isActive()){
-				
-				return ANONY_WPML_HELP::getTranslatedTerm($term_id, $taxonomy);
+		static function getTermBy( $term_id, $taxonomy ) {
+
+			if ( ANONY_WPML_HELP::isActive() ) {
+
+				return ANONY_WPML_HELP::getTranslatedTerm( $term_id, $taxonomy );
 			}
-			
+
 			return get_term_by( 'id', $term_id, $taxonomy );
-			
+
 		}
 
 		/**
 		 * Create pagination for terms list
-		 * @param string $taxonomy 
-		 * @param int    $per_page 
+		 *
+		 * @param string $taxonomy
+		 * @param int    $per_page
 		 */
-		static function termsPagination($taxonomy, $per_page){
+		static function termsPagination( $taxonomy, $per_page ) {
 			/*
 				You should filter get_term args where the code that gets terms exists
-				
-		
+
+
 				$page = ( get_query_var('paged') ) ? get_query_var( 'paged' ) : 1;
-				
+
 				$per_page = 50;
-				
+
 				$offset = ( $page-1 ) * $per_page;
-				
+
 				$args['number'] = $per_page;
 				$args['offset'] = $offset;
-				
+
 			*/
-			$pagination = '';
+			$pagination  = '';
 			$total_terms = wp_count_terms( $taxonomy );
-	    	$pages = ceil($total_terms/$per_page);
+			$pages       = ceil( $total_terms / $per_page );
 
 			// if there's more than one page
-			if( $pages > 1 ):
+			if ( $pages > 1 ) :
 				$pagination .= '<ul class="pagination col-md-8 col-sm-8 col-xs-8" style="margin:auto;float: none;display: block;">';
 
-				for ($pagecount=1; $pagecount <= $pages; $pagecount++):
-					$pagination .= '<li><a href="'.get_permalink().'page/'.$pagecount.'/">'.$pagecount.'</a></li>';
+				for ( $pagecount = 1; $pagecount <= $pages; $pagecount++ ) :
+					$pagination .= '<li><a href="' . get_permalink() . 'page/' . $pagecount . '/">' . $pagecount . '</a></li>';
 				endfor;
 
 				$pagination .= '</ul>';
 			endif;
 		}
 
-		static function group_terms_by_parent ($args) {
+		/**
+		 * Groups terms by parent.
+		 *
+		 * @param  array $args Arguments required for get_terms.
+		 * @return array Groupped terms
+		 */
+		static function group_terms_by_parent( $args ) {
 
-			$categories = get_terms($args);
-            
-            // Get all parents values. (May have duplicates).
-		    $duplicate_parents = array_column($categories,'parent');
-		    
-		    // Remove duplicates.
-		    $unique_parents    = array_unique($duplicate_parents);
-		    
-		    $grouped           = array();
-		    
-		    foreach($unique_parents as $parent){
+			$categories = get_terms( $args );
 
-		        foreach($categories as $category){
-		        
-		            if ($category->parent === $parent) {
-		                
-		                if ( !isset($grouped[$parent]) ) {
-		                    $grouped[$parent]=array();
-		                }
-		                
-		                $grouped[$parent][] = $category;
-		            }
-		            
-		        }
-		        
-		    }
+			// Get all parents values. (May have duplicates).
+			$duplicate_parents = array_column( $categories, 'parent' );
 
-		    return $grouped;
+			// Remove duplicates.
+			$unique_parents = array_unique( $duplicate_parents );
+
+			$grouped = array();
+
+			foreach ( $unique_parents as $parent ) {
+
+				foreach ( $categories as $category ) {
+
+					if ( $category->parent === $parent ) {
+
+						if ( ! isset( $grouped[ $parent ] ) ) {
+							$grouped[ $parent ] = array();
+						}
+
+						$grouped[ $parent ][] = $category;
+					}
+				}
+			}
+
+			return $grouped;
 		}
-		
+
+		/**
+		 * Groups top level terms with their children.
+		 *
+		 * @param  array $args Arguments required for get_terms.
+		 * @return array Groupped terms by IDs.
+		 */
+		static function top_level_terms_children( $args ) {
+
+			$args['parent'] = 0;
+			$categories     = get_terms( $args );
+
+			$grouped = array();
+
+			foreach ( $categories as $category ) {
+
+				$children = get_term_children( $category->term_id, $args['taxonomy'] );
+
+				if ( empty( $children ) ) {
+					$grouped[ $category->term_id ] = array( $category->term_id );
+				} else {
+					$grouped[ $category->term_id ] = $children;
+				}
+			}
+
+			return $grouped;
+		}
+
 	}
 }

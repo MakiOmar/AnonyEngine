ff906cfbf59ccd92715f727bc4f3ff38441b3764
unknown <mo7amed.maki@gmail.com>
2022-03-03 16:23:37 +0200

    Comply to WordPress standard

--- helpme/php/phpoffice-help.php
+++ helpme/php/phpoffice-help.php
@@ -17,38 +17,38 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
 if ( ! class_exists( 'ANONY_PHPOFFICE_HELP' ) ) {
-    /**
-     * PHPOFFICE helpers class
-     *
-     * @package    AnonyEngine helpers
-     * @author     Makiomar <info@makior.com>
-     * @license    https://makiomar.com AnonyEngine Licence
-     * @link       https://makiomar.com
-     */
-    class ANONY_PHPOFFICE_HELP extends ANONY_HELP {
+	/**
+	 * PHPOFFICE helpers class
+	 *
+	 * @package    AnonyEngine helpers
+	 * @author     Makiomar <info@makior.com>
+	 * @license    https://makiomar.com AnonyEngine Licence
+	 * @link       https://makiomar.com
+	 */
+	class ANONY_PHPOFFICE_HELP extends ANONY_HELP {
 
-        /**
-         * Writes array content into xlsx spread sheet.
-         *
-         * @param  array $array Data array.
-         * @return void
-         */
-        public static function array_to_spreadsheet( $array ) {
+		/**
+		 * Writes array content into xlsx spread sheet.
+		 *
+		 * @param  array $array Data array.
+		 * @return void
+		 */
+		public static function array_to_spreadsheet( $array ) {
 
-            $spreadsheet = new Spreadsheet();
-            $spreadsheet->setActiveSheetIndex(0);
-            $spreadsheet->getActiveSheet()->fromArray($array, NULL, 'A1');
-            $spreadsheet->getActiveSheet()->setTitle("Spread Sheet");
+			$spreadsheet = new Spreadsheet();
+			$spreadsheet->setActiveSheetIndex( 0 );
+			$spreadsheet->getActiveSheet()->fromArray( $array, null, 'A1' );
+			$spreadsheet->getActiveSheet()->setTitle( 'Spread Sheet' );
 
-            header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-            header('Content-Disposition: attachment;filename="test.xlsx"');
-            header('Cache-Control: max-age=0');
-            header('Cache-Control: max-age=1');
-            header('Cache-Control: cache, must-revalidate');
-            header('Pragma: public');
-            $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
-            $writer->save('php://output');
+			header( 'Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' );
+			header( 'Content-Disposition: attachment;filename="test.xlsx"' );
+			header( 'Cache-Control: max-age=0' );
+			header( 'Cache-Control: max-age=1' );
+			header( 'Cache-Control: cache, must-revalidate' );
+			header( 'Pragma: public' );
+			$writer = IOFactory::createWriter( $spreadsheet, 'Xlsx' );
+			$writer->save( 'php://output' );
 
-        }
-    }
+		}
+	}
 }

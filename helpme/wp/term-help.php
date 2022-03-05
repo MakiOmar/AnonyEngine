75adf0e72a54ca0dcdf219715536c6128e4aa996
unknown <mo7amed.maki@gmail.com>
2022-03-05 18:56:01 +0200

    comply to WordPress standards

--- helpme/wp/term-help.php
+++ helpme/wp/term-help.php
@@ -1,13 +1,26 @@
 <?php
 /**
- * WP terms helpers class
+ * Terms helpers
  *
- * @package Anonymous theme
- * @author Makiomar
- * @link http://makiomar.com
+ * PHP version 7.3 Or Later
+ *
+ * @package  AnonyEngine helpers
+ * @author   Makiomar <info@makior.com>
+ * @license  https://makiomar.com AnonyEngine Licence
+ * @link     https://makiomar.com/anonyengine
  */
 
+defined( 'ABSPATH' ) || die(); // Exit if accessed direct.
+
 if ( ! class_exists( 'ANONY_TERM_HELP' ) ) {
+	/**
+	 * Terms helpers' class
+	 *
+	 * @package    AnonyEngine helpers
+	 * @author     Makiomar <info@makior.com>
+	 * @license    https://makiomar.com AnonyEngine Licence
+	 * @link       https://makiomar.com
+	 */
 	class ANONY_TERM_HELP extends ANONY_HELP {
 
 		/**
@@ -15,11 +28,10 @@ if ( ! class_exists( 'ANONY_TERM_HELP' ) ) {
 		 *
 		 * Use instead of get_terms for admin purpuses.
 		 *
-		 * @param  string $taxonomy Taxonomy to get terms from
-		 * @return arrsy             An indexed array of terms slugs
+		 * @param  string $taxonomy Taxonomy to get terms from.
+		 * @return array             An indexed array of terms slugs.
 		 */
-
-		static function queryTermsinAdmin( $taxonomy ) {
+		public static function queryTermsinAdmin( $taxonomy ) {
 
 			$terms = ANONY_ARRAY_HELP::ObjToAssoc(
 				self::queryTermsByTaxonomy( $taxonomy ),
@@ -30,14 +42,23 @@ if ( ! class_exists( 'ANONY_TERM_HELP' ) ) {
 		}
 
 		/**
-		 * Query terms slug names by taxonomy
+		 * Query terms slug names by taxonomy.
 		 *
-		 * @param  string $taxonomy taxonomy to get terms from
-		 * @return array             An array of terms objects contains only slug name
+		 * @param  string $taxonomy taxonomy to get terms from.
+		 * @return array             An array of terms objects contains only slug name.
 		 */
-		static function queryTermsSlugsByTaxonomy( $taxonomy ) {
+		public static function queryTermsSlugsByTaxonomy( $taxonomy ) {
 			global $wpdb;
-			$query = "SELECT DISTINCT 
+
+			$cache_key = 'anony_terms_slug_by_taxonomy_' . $taxonomy;
+
+			$result = wp_cache_get( $cache_key );
+
+			if ( false === $result ) {
+
+				$result = $wpdb->get_results(
+					$wpdb->prepare(
+						"SELECT DISTINCT 
 						t.slug 
 						FROM 
 							$wpdb->terms t 
@@ -46,24 +67,37 @@ if ( ! class_exists( 'ANONY_TERM_HELP' ) ) {
 						ON 
 							tax.term_id = t.term_id 
 						WHERE 
-							tax.taxonomy = '$taxonomy'";
+							tax.taxonomy = %s ",
+						$taxonomy
+					)
+				);
 
-			$result = $wpdb->get_results( $query );
+				wp_cache_set( $cache_key, $result );
 
-			ANONY_WPDEBUG_HELP::printDbErrors( $result );
+				ANONY_WPDEBUG_HELP::printDbErrors( $result );
+			}
 
 			return $result;
 		}
-
 		/**
-		 * Query terms by taxonomy
+		 * Query terms by taxonomy.
 		 *
-		 * @param  string $taxonomy taxonomy to get terms from
-		 * @return array             An array of terms objects
+		 * @param  string $taxonomy taxonomy to get terms from.
+		 * @param  string $operator Query operator.
+		 * @return array            An array of terms objects.
 		 */
-		static function queryTermsByTaxonomy( $taxonomy, $operator = '=' ) {
+		public static function queryTermsByTaxonomy( $taxonomy, $operator = '=' ) {
 			global $wpdb;
-			$query = "SELECT 
+
+			$cache_key = 'anony_terms_by_taxonomy_' . $taxonomy;
+
+			$result = wp_cache_get( $cache_key );
+
+			if ( false === $result ) {
+
+				$result = $wpdb->get_results(
+					$wpdb->prepare(
+						"SELECT 
 							* 
 						FROM 
 							$wpdb->terms t 
@@ -72,11 +106,16 @@ if ( ! class_exists( 'ANONY_TERM_HELP' ) ) {
 						ON 
 							tax.term_id = t.term_id 
 						WHERE 
-							tax.taxonomy $operator '$taxonomy'";
+							tax.taxonomy %s %s",
+						$operator,
+						$taxonomy
+					)
+				);
 
-			$result = $wpdb->get_results( $query );
+				wp_cache_set( $cache_key, $result );
 
-			ANONY_WPDEBUG_HELP::printDbErrors( $result );
+				ANONY_WPDEBUG_HELP::printDbErrors( $result );
+			}
 
 			return $result;
 		}
@@ -86,12 +125,11 @@ if ( ! class_exists( 'ANONY_TERM_HELP' ) ) {
 		 *
 		 * Use instead of get_terms for admin purpuses.
 		 *
-		 * @param  string $tax    Taxonomy to get terms from
+		 * @param  string $tax    Taxonomy to get terms from.
 		 * @param  string $fields Fields to fetch.
-		 * @return array             array of terms (id, name, slug)
+		 * @return array          An array of terms (id, name, slug).
 		 */
-
-		static function wpTermQuery( $tax, $fields ) {
+		public static function wpTermQuery( $tax, $fields ) {
 			/**
 			 * 'fields' to return Accepts:
 			 * 'all' (returns an array of complete term objects),
@@ -105,15 +143,15 @@ if ( ! class_exists( 'ANONY_TERM_HELP' ) ) {
 			 * 'id=>name' (returns an associative array with ids as keys, term names as values), or
 			 * 'id=>slug' (returns an associative array with ids as keys, term slugs as values)
 			 */
-			$termsObject = new WP_Term_Query(
+			$terms_object = new WP_Term_Query(
 				array(
 					'taxonomy' => $tax,
 					'fields'   => $fields,
 				)
 			);
 
-			if ( ! empty( $termsObject->terms ) ) {
-				return $termsObject->terms;
+			if ( ! empty( $terms_object->terms ) ) {
+				return $terms_object->terms;
 			}
 
 			return '';
@@ -121,11 +159,11 @@ if ( ! class_exists( 'ANONY_TERM_HELP' ) ) {
 		/**
 		 * Gets post terms from child up to first parent
 		 *
-		 * @param  int  $id   Term id
-		 * @param  type $tax  Term taxonomy
-		 * @return string     Dash separated terms IDs
+		 * @param  int  $id   Term's ID.
+		 * @param  type $tax  Term taxonomy.
+		 * @return string     Dash separated terms IDs.
 		 */
-		static function termParents( $id, $tax ) {
+		public static function termParents( $id, $tax ) {
 			$terms  = '';
 			$parent = get_term( $id, $tax );
 
@@ -134,7 +172,7 @@ if ( ! class_exists( 'ANONY_TERM_HELP' ) ) {
 
 			$terms .= $parent->term_id;
 
-			if ( $parent->parent && ( $parent->parent != $parent->term_id ) ) {
+			if ( $parent->parent && ( $parent->parent !== $parent->term_id ) ) {
 
 				$terms .= '-' . self::termParents( $parent->parent, $tax );
 
@@ -143,18 +181,28 @@ if ( ! class_exists( 'ANONY_TERM_HELP' ) ) {
 		}
 
 		/**
-		 * Delete all terms connected supplied taxonomies. Can also delete taxonomy
+		 * Delete all terms connected supplied taxonomies. Can also delete taxonomy.
 		 *
 		 * @param array $taxonomies Array of taxonomies to delete terms connected to.
-		 * @param bool  $dlt_tax    Boolean to decide weather to delete a taxonomy. default false
+		 * @param bool  $delete_taxonomy    Boolean to decide weather to delete a taxonomy. default yes.
 		 */
-		static function deleteTerms( $taxonomies, $dlt_tax = false ) {
+		public static function deleteTerms( $taxonomies, $delete_taxonomy = 'yes' ) {
 			global $wpdb;
 			foreach ( $taxonomies as $taxonomy ) {
-				// Prepare & excecute SQL, Delete Terms
-				$result = $wpdb->get_results( $wpdb->prepare( "DELETE t.*, tt.* FROM $wpdb->terms AS t INNER JOIN $wpdb->term_taxonomy AS tt ON t.term_id = tt.term_id WHERE tt.taxonomy IN ('%s')", $taxonomy ) );
-				// Delete Taxonomy
-				if ( $dlt_tax ) {
+				// Prepare & excecute SQL, Delete Terms.
+				$cache_key = 'anony_delete_terms_by_taxonomy_' . $taxonomy;
+
+				$result = wp_cache_get( $cache_key );
+
+				if ( false === $result ) {
+
+					$result = $wpdb->get_results( $wpdb->prepare( "DELETE t.*, tt.* FROM $wpdb->terms AS t INNER JOIN $wpdb->term_taxonomy AS tt ON t.term_id = tt.term_id WHERE tt.taxonomy IN (%s)", $taxonomy ) );
+
+					wp_cache_set( $cache_key, $result );
+				}
+
+				// Delete Taxonomy.
+				if ( 'yes' === $delete_taxonomy ) {
 					$wpdb->delete( $wpdb->term_taxonomy, array( 'taxonomy' => $taxonomy ), array( '%s' ) );
 				}
 			}
@@ -163,11 +211,11 @@ if ( ! class_exists( 'ANONY_TERM_HELP' ) ) {
 		/**
 		 * Get term by id
 		 *
-		 * @param int    $term_id
-		 * @param string $taxonomy
-		 * @return mixed
+		 * @param int    $term_id  Term's ID.
+		 * @param string $taxonomy Taxonomy slug.
+		 * @return mixed (WP_Term|array|false) WP_Term instance (or array) on success, depending on the $output value. False if $taxonomy does not exist or $term was not found.
 		 */
-		static function getTermBy( $term_id, $taxonomy ) {
+		public static function getTermBy( $term_id, $taxonomy ) {
 
 			if ( ANONY_WPML_HELP::isActive() ) {
 
@@ -179,31 +227,31 @@ if ( ! class_exists( 'ANONY_TERM_HELP' ) ) {
 		}
 
 		/**
-		 * Create pagination for terms list
+		 * Create pagination for terms list.
 		 *
-		 * @param string $taxonomy
-		 * @param int    $per_page
+		 * @param string $taxonomy Taxonomy slug.
+		 * @param int    $posts_per_page Number of posts per page.
 		 */
-		static function termsPagination( $taxonomy, $per_page ) {
+		public static function termsPagination( $taxonomy, $posts_per_page ) {
 			/*
 				You should filter get_term args where the code that gets terms exists
 
 
 				$page = ( get_query_var('paged') ) ? get_query_var( 'paged' ) : 1;
 
-				$per_page = 50;
+				$posts_per_page = 50;
 
-				$offset = ( $page-1 ) * $per_page;
+				$offset = ( $page-1 ) * $posts_per_page;
 
-				$args['number'] = $per_page;
+				$args['number'] = $posts_per_page;
 				$args['offset'] = $offset;
 
 			*/
 			$pagination  = '';
 			$total_terms = wp_count_terms( $taxonomy );
-			$pages       = ceil( $total_terms / $per_page );
+			$pages       = ceil( $total_terms / $posts_per_page );
 
-			// if there's more than one page
+			// if there's more than one page.
 			if ( $pages > 1 ) :
 				$pagination .= '<ul class="pagination col-md-8 col-sm-8 col-xs-8" style="margin:auto;float: none;display: block;">';
 
@@ -221,7 +269,7 @@ if ( ! class_exists( 'ANONY_TERM_HELP' ) ) {
 		 * @param  array $args Arguments required for get_terms.
 		 * @return array Groupped terms
 		 */
-		static function group_terms_by_parent( $args ) {
+		public static function group_terms_by_parent( $args ) {
 
 			$categories = get_terms( $args );
 
@@ -257,7 +305,7 @@ if ( ! class_exists( 'ANONY_TERM_HELP' ) ) {
 		 * @param  array $args Arguments required for get_terms.
 		 * @return array Groupped terms by IDs.
 		 */
-		static function top_level_terms_children( $args ) {
+		public static function top_level_terms_children( $args ) {
 
 			$args['parent'] = 0;
 			$categories     = get_terms( $args );
@@ -283,30 +331,30 @@ if ( ! class_exists( 'ANONY_TERM_HELP' ) ) {
 		 *
 		 * @param  array $args       Arguments required for get_terms.
 		 * @param  array $attributes Select input attributes.
+		 *
 		 * @return void
 		 */
-		static function top_level_terms_option_groups( $args, $attributes ) {
+		public static function top_level_terms_option_groups( $args, $attributes ) {
 
 			$groups = self::top_level_terms_children( $args );
 
-			$name = trim($attributes['name']);
-			$class = trim( 'terms-options-group '. $attributes['class'] );
+			$name  = trim( $attributes['name'] );
+			$class = trim( 'terms-options-group ' . $attributes['class'] );
 
 			$select = "<select name='{$name}' class='{$class}'>";
 
-		    $select.= "<option value='-1'>".esc_html__('Select category', ABBL_DOMAIN)."</option>";
-		    if(!empty($grouped)){
-		        foreach($grouped as $parent => $children){
-		            $select .= '<optgroup label="'. get_term($parent)->name .'">';
-		            
-		            foreach ($children as $child_id) {
-		                $select.= "<option value='".get_term($child_id)->term_id."'".selected(intval($requested_cat), get_term($child_id)->term_id, false).">".get_term($child_id)->name."</option>";
-		            }
-		            
-		            
-		            $select .= '</optgroup>';
-		        }
-		    }
+			$select .= "<option value='-1'>" . esc_html__( 'Select category', 'anonyengine' ) . '</option>';
+			if ( ! empty( $grouped ) ) {
+				foreach ( $grouped as $parent => $children ) {
+					$select .= '<optgroup label="' . get_term( $parent )->name . '">';
+
+					foreach ( $children as $child_id ) {
+						$select .= "<option value='" . get_term( $child_id )->term_id . "'" . selected( intval( $requested_cat ), get_term( $child_id )->term_id, false ) . '>' . get_term( $child_id )->name . '</option>';
+					}
+
+					$select .= '</optgroup>';
+				}
+			}
 
 		}
 

fc4fc72ce801607d1c27f03f44d8a5d73c49d389
unknown <mo7amed.maki@gmail.com>
2022-03-27 21:49:12 +0200

    Comply to WordPress standards

--- forms/classes/actions/class-anony-insert-post.php
+++ forms/classes/actions/class-anony-insert-post.php
@@ -14,27 +14,41 @@ defined( 'ABSPATH' ) || die(); // Exit if accessed directly.
 
 if ( ! class_exists( 'ANONY_Insert_Post' ) ) {
 
-    /**
-     * AnonyEngine post insertion class.
-     *
-     * @package    AnonyEngine
-     * @author     Makiomar <info@makior.com>
-     * @license    https:// makiomar.com AnonyEngine Licence.
-     * @link       https:// makiomar.com
-     */
-    class ANONY_Insert_Post{
+	/**
+	 * AnonyEngine post insertion class.
+	 *
+	 * @package    AnonyEngine
+	 * @author     Makiomar <info@makior.com>
+	 * @license    https:// makiomar.com AnonyEngine Licence.
+	 * @link       https:// makiomar.com
+	 */
+	class ANONY_Insert_Post {
 
-        /**
-         * Constructor.
-         *
-         * @param array $validated_data $_POST after validation.
-         * @param array $action_data Fields mapping as ( 'wp_key' => 'form_key' ). While wp_key stands for the original key that should be passed to wp_inser_post arguments.
-         */
-        public function __construct( $validated_data, $action_data ) {
+		/**
+		 * Required arguments for post insertion.
+		 */
+		const REQUIRED_ARGUMENTS = array( 'post_type', 'post_status', 'post_title' );
 
-            error_log( print_r( $action_data, true ) );
+		/**
+		 * Constructor.
+		 *
+		 * @param array $validated_data $_POST after validation.
+		 * @param array $action_data Fields mapping as ( 'wp_key' => 'form_key' ). While wp_key stands for the original key that should be passed to wp_inser_post arguments.
+		 */
+		public function __construct( $validated_data, $action_data ) {
 
-        }
-    }
+			// Argumnets sent from the form.
+			$posted_required_arguments = array_intersect_key( $validated_data, $action_data );
+
+			if ( ! ANONY_HELP::isset_not_empty( $posted_required_arguments['post_type'] ) || ! ANONY_HELP::isset_not_empty( $posted_required_arguments['post_status'] ) || ! ANONY_HELP::isset_not_empty( $posted_required_arguments['post_title'] ) ) {
+
+			}
+
+		}
+
+
+
+
+	}
 
 }

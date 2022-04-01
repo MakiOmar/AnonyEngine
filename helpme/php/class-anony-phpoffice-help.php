23ae515398adc19955713c85096494cdde567485
unknown <mo7amed.maki@gmail.com>
2022-04-01 21:27:39 +0200

    Comply to WordPress standards

--- helpme/php/class-anony-phpoffice-help.php
+++ helpme/php/class-anony-phpoffice-help.php
@@ -10,7 +10,7 @@
  * @link     https://makiomar.com/anonyengine
  */
 
-defined( 'ABSPATH' ) || die(); // Exit if accessed direct.
+defined( 'ABSPATH' ) || die(); // Exit if accessed direct..
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
@@ -35,10 +35,10 @@ if ( ! class_exists( 'ANONY_PHPOFFICE_HELP' ) ) {
 		 */
 		public static function array_to_spreadsheet( $array ) {
 
-			// Create new Spreadsheet object
+			// Create new Spreadsheet object.
 			$spreadsheet = new Spreadsheet();
 
-			// Set document properties
+			// Set document properties.
 			$spreadsheet->getProperties()->setCreator( 'Maarten Balliauw' )
 				->setLastModifiedBy( 'Maarten Balliauw' )
 				->setTitle( 'Office 2007 XLSX Test Document' )
@@ -47,36 +47,36 @@ if ( ! class_exists( 'ANONY_PHPOFFICE_HELP' ) ) {
 				->setKeywords( 'office 2007 openxml php' )
 				->setCategory( 'Test result file' );
 
-			// Add some data
+			// Add some data.
 			$spreadsheet->setActiveSheetIndex( 0 )
 				->setCellValue( 'A1', 'Hello' )
 				->setCellValue( 'B2', 'world!' )
 				->setCellValue( 'C1', 'Hello' )
 				->setCellValue( 'D2', 'world!' );
 
-			// Miscellaneous glyphs, UTF-8
+			// Miscellaneous glyphs, UTF-8.
 			$spreadsheet->setActiveSheetIndex( 0 )
 				->setCellValue( 'A4', 'Miscellaneous glyphs' )
 				->setCellValue( 'A5', 'éàèùâêîôûëïüÿäöüç' );
 
-			// Rename worksheet
+			// Rename worksheet.
 			$spreadsheet->getActiveSheet()->setTitle( 'Simple' );
 
-			// Set active sheet index to the first sheet, so Excel opens this as the first sheet
+			// Set active sheet index to the first sheet, so Excel opens this as the first sheet.
 			$spreadsheet->setActiveSheetIndex( 0 );
 
-			// Redirect output to a client’s web browser (Xlsx)
+			// Redirect output to a client’s web browser (Xlsx).
 			header( 'Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' );
 			header( 'Content-Disposition: attachment;filename="01simple.xlsx"' );
 			header( 'Cache-Control: max-age=0' );
-			// If you're serving to IE 9, then the following may be needed
+			// If you're serving to IE 9, then the following may be needed.
 			header( 'Cache-Control: max-age=1' );
 
-			// If you're serving to IE over SSL, then the following may be needed
-			header( 'Expires: Mon, 26 Jul 1997 05:00:00 GMT' ); // Date in the past
-			header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT' ); // always modified
-			header( 'Cache-Control: cache, must-revalidate' ); // HTTP/1.1
-			header( 'Pragma: public' ); // HTTP/1.0
+			// If you're serving to IE over SSL, then the following may be needed.
+			header( 'Expires: Mon, 26 Jul 1997 05:00:00 GMT' ); // Date in the past.
+			header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT' ); // always modified.
+			header( 'Cache-Control: cache, must-revalidate' ); // HTTP/1.1.
+			header( 'Pragma: public' ); // HTTP/1.0.
 
 			$writer = IOFactory::createWriter( $spreadsheet, 'Xlsx' );
 			$writer->save( 'php://output' );
@@ -91,24 +91,24 @@ if ( ! class_exists( 'ANONY_PHPOFFICE_HELP' ) ) {
 		 * @return array An array of read data from an excel sheet.
 		 */
 		public static function read_excel_data( $input_name ) {
-
-			if ( empty( $_FILES ) || ! isset( $_FILES ) || ! isset( $_FILES[ $input_name ] ) || empty( $_FILES[ $input_name ]['tmp_name'] ) ) {
+			$sent_files = wp_unslash( $_FILES );
+			if ( empty( $sent_files ) || ! isset( $sent_files ) || ! isset( $sent_files[ $input_name ] ) || empty( $sent_files[ $input_name ]['tmp_name'] ) ) {
 				return;
 			}
 
-			$inputFileType = IOFactory::identify( $_FILES[ $input_name ]['tmp_name'] );
+			$input_file_type = IOFactory::identify( $sent_files[ $input_name ]['tmp_name'] );
 
-			$reader = PhpOffice\PhpSpreadsheet\IOFactory::createReader( $inputFileType );
+			$reader = PhpOffice\PhpSpreadsheet\IOFactory::createReader( $input_file_type );
 
-			// This is to not read empty rows
-			// $reader->setReadDataOnly(true);
+			// This is to not read empty rows.
+			// $reader->setReadDataOnly(true);.
 			$reader->setReadEmptyCells( false );
 
-			$spreadsheet = $reader->load( $_FILES[ $input_name ]['tmp_name'] );
+			$spreadsheet = $reader->load( $sent_files[ $input_name ]['tmp_name'] );
 			$worksheet   = $spreadsheet->getActiveSheet();
 			$rows        = $worksheet->toArray();
 
-			// Remove Nulls (empty cells)
+			// Remove Nulls (empty cells).
 			$temp = array();
 			foreach ( $rows as $row ) {
 				$row = array_filter( $row );

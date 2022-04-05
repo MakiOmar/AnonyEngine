553fdb3584a459604e7cb756085e54cd304ea023
unknown <mo7amed.maki@gmail.com>
2022-04-05 19:52:30 +0200

    Comply to WordPress standards

--- helpme/php/class-anony-date-help.php
+++ helpme/php/class-anony-date-help.php
@@ -6,20 +6,31 @@
  * @author Makiomar
  * @link http://makiomar.com
  */
+
+defined( 'ABSPATH' ) || die();
+
 if ( ! class_exists( 'ANONY_DATE_HELP' ) ) {
+	/**
+	 * PHP date helpers class
+	 *
+	 * @package    AnonyEngine
+	 * @author     Makiomar <info@makior.com>
+	 * @license    https://makiomar.com AnonyEngine Licence
+	 * @link       https://makiomar.com
+	 */
 	class ANONY_DATE_HELP extends ANONY_HELP {
 		/**
 		 * Check if valid date
 		 *
-		 * @param string $date
+		 * @param string $date Date to be checked.
 		 * @return boolean true on success otherwise false
 		 */
-		static function isDate( $date ) {
-			// date example mm-dd-year -> 09-25-2012
+		public static function is_date( $date ) {
+			// date example mm-dd-year -> 09-25-2012.
 			$datechunks = explode( '-', $date );
-			if ( sizeof( $datechunks ) == 3 ) {
+			if ( count( $datechunks ) === 3 ) {
 				if ( is_numeric( $datechunks[0] ) && is_numeric( $datechunks[1] ) && is_numeric( $datechunks[2] ) ) {
-					// now check if its a valid date
+					// now check if its a valid date.
 					if ( checkdate( $datechunks[0], $datechunks[1], $datechunks[2] ) ) {
 						return true;
 					} else {
@@ -34,15 +45,15 @@ if ( ! class_exists( 'ANONY_DATE_HELP' ) ) {
 		}
 
 		/**
-		 * Convert date/time from timezone to another
+		 * Convert date/time from timezone to another.
 		 *
 		 * @param  string $date         required to be converted.
 		 * @param  string $timezone     current timezone to be converted from.
 		 * @param  string $timezone_to  current timezone to be converted to.
 		 * @param  string $format       format to be converted to.
-		 * @return string                 converted date.
+		 * @return string               converted date.
 		 */
-		static function convertDateFromTimezone( $date, $timezone, $timezone_to, $format = 'Y-m-d H:i' ) {
+		public static function convert_date_from_timezone( $date, $timezone, $timezone_to, $format = 'Y-m-d H:i' ) {
 
 			$date = new DateTime( $date, new DateTimeZone( $timezone ) );
 
@@ -52,23 +63,23 @@ if ( ! class_exists( 'ANONY_DATE_HELP' ) ) {
 		}
 
 		/**
-		 * Get formated date/time difference e.g. 2 days and 3 hours and 40 minutes and 25 seconds
+		 * Get formated date/time difference e.g. 2 days and 3 hours and 40 minutes and 25 seconds.
 		 *
-		 * @param  string $timeStamp         the timestamp you want to calculate difference from.
-		 * @param  string $timezone          timezone you want to use for conversion.
-		 * @var    object   $setTimeZone       an object of DateTimeZone.
+		 * @param  string $time_stamp         the timestamp you want to calculate difference from.
+		 * @param  string $time_zone          timezone you want to use for conversion.
+		 * @var    object   $set_time_zone       an object of DateTimeZone.
 		 * @var    string   $converted_date    Store formated timestamp.
 		 * @var    object   $date              object of formated date/time according to timezone.
 		 * @var    object   $current_date      object of current date/time.
 		 * @var    string   $diff              stors formated date/time difference.
 		 * @return string
 		 */
-		static function getTimeDifference( $timeStamp, $time_zone ) {
-			$setTimeZone = new DateTimeZone( $time_zone );
+		public static function get_time_difference( $time_stamp, $time_zone ) {
+			$set_time_zone = new DateTimeZone( $time_zone );
 
-			$converted_date = date( 'Y-m-d H:i:s', $timeStamp );
+			$converted_date = gmdate( 'Y-m-d H:i:s', $time_stamp );
 
-			$date = DateTime::createFromFormat( 'Y-m-d H:i:s', $converted_date, $setTimeZone );
+			$date = DateTime::createFromFormat( 'Y-m-d H:i:s', $converted_date, $set_time_zone );
 
 			$current_date = new DateTime();
 
@@ -80,12 +91,12 @@ if ( ! class_exists( 'ANONY_DATE_HELP' ) ) {
 		/**
 		 * Calculate the difference between to dates in days.
 		 *
-		 * @param  string $new_date Date/Time string
-		 * @param  string $old_date Date/Time string
+		 * @param  string $new_date Date/Time string.
+		 * @param  string $old_date Date/Time string.
 		 * @param  int    $round    The optional number of decimal digits to round to.(Round percision).
-		 * @return float            The date difference
+		 * @return float            The date difference.
 		 */
-		static function dateDiffInDays( $new_date, $old_date, $round = 3 ) {
+		public static function date_diff_in_days( $new_date, $old_date, $round = 3 ) {
 			$new_date_timestamp = strtotime( $new_date );
 			$old_date_timestamp = strtotime( $old_date );
 

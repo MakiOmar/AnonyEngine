9e6fa66ff357561676bca4334006dbb1e7de795c
unknown <mo7amed.maki@gmail.com>
2022-04-15 23:18:01 +0200

    Comply to WordPress Standard

--- helpme/wp/class-anony-wp-json-help.php
+++ helpme/wp/class-anony-wp-json-help.php
@@ -1,16 +1,35 @@
 <?php
 /**
- * WP JSON helpers class
+ * WP JSON helpers.
  *
- * @package Anonymous theme
- * @author Makiomar
- * @link http://makiomar.com
+ * PHP version 7.3 Or Later.
+ *
+ * @package  AnonyEngine.
+ * @author   Makiomar <info@makior.com>.
+ * @license  https:// makiomar.com AnonyEngine Licence.
+ * @link     https:// makiomar.com/anonyengine
  */
 
 if ( ! class_exists( 'ANONY_JSON_HELP' ) ) {
-	class ANONY_JSON_HELP extends ANONY_HELP {
+	/**
+	 * WP JSON helpers class.
+	 *
+	 * PHP version 7.3 Or Later.
+	 *
+	 * @package  AnonyEngine.
+	 * @author   Makiomar <info@makior.com>.
+	 * @license  https:// makiomar.com AnonyEngine Licence.
+	 * @link     https:// makiomar.com/anonyengine
+	 */
+	class ANONY_Wp_Json_Help extends ANONY_HELP {
 
-		static function jsonizeArrayForJs( array $arr ) {
+		/**
+		 * WP JSON helpers class.
+		 *
+		 * @param array $array To be converted array.
+		 * @return string JSON formated string.
+		 */
+		public static function to_js_json( array $array ) {
 			return wp_json_encode( $arr, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES );
 		}
 

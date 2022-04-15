66c3da81fe3471b7264b6403988d989701a6fbeb
unknown <mo7amed.maki@gmail.com>
2022-04-15 23:23:47 +0200

    Rename file

---
+++ helpme/wp/class-anony-wp-array-help.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * WP array helpers.
+ *
+ * PHP version 7.3 Or Later.
+ *
+ * @package  AnonyEngine.
+ * @author   Makiomar <info@makior.com>.
+ * @license  https:// makiomar.com AnonyEngine Licence.
+ * @link     https:// makiomar.com/anonyengine
+ */
+
+defined( 'ABSPATH' ) || die();
+
+if ( ! class_exists( 'ANONY_Wp_Array_Help' ) ) {
+	/**
+	 * WP array helpers class.
+	 *
+	 * PHP version 7.3 Or Later.
+	 *
+	 * @package  AnonyEngine.
+	 * @author   Makiomar <info@makior.com>.
+	 * @license  https:// makiomar.com AnonyEngine Licence.
+	 * @link     https:// makiomar.com/anonyengine
+	 */
+	class ANONY_Wp_Array_Help extends ANONY_HELP {
+		/**
+		 * Similar to wp_parse_args() just a bit extended to work with multidimensional arrays :).
+		 *
+		 * @param array $a The default args.
+		 * @param array $b To be parsed args.
+		 * @return array   Parsed array.
+		 */
+		public static function parse_args( &$a, $b ) {
+			$a      = (array) $a;
+			$b      = (array) $b;
+			$result = $b;
+			foreach ( $a as $k => &$v ) {
+				if ( is_array( $v ) && isset( $result[ $k ] ) ) {
+					$result[ $k ] = self::parse_args( $v, $result[ $k ] );
+				} else {
+					$result[ $k ] = $v;
+				}
+			}
+			return $result;
+		}
+
+	}
+}

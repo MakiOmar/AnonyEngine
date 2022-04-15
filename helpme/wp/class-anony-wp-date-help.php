eab7d524c73ac78ad898d6469f0e7b568970ac1c
unknown <mo7amed.maki@gmail.com>
2022-04-16 00:05:01 +0200

    Rename class

---
+++ helpme/wp/class-anony-wp-date-help.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * WP date helpers.
+ *
+ * PHP version 7.3 Or Later.
+ *
+ * @package  AnonyEngine
+ * @author   Makiomar <info@makior.com>
+ * @license  https://makiomar.com AnonyEngine Licence
+ * @link     https://makiomar.com/anonyengine
+ */
+
+defined( 'ABSPATH' ) || die(); // Exit if accessed direct.
+
+if ( ! class_exists( 'ANONY_Wp_Date_Help' ) ) {
+
+	/**
+	 * WP date helpers class.
+	 *
+	 * PHP version 7.3 Or Later.
+	 *
+	 * @package  AnonyEngine
+	 * @author   Makiomar <info@makior.com>
+	 * @license  https://makiomar.com AnonyEngine Licence
+	 * @link     https://makiomar.com/anonyengine
+	 */
+	class ANONY_Wp_Date_Help extends ANONY_HELP {
+
+		/**
+		 * Check if a date is in the past.
+		 *
+		 * @param string $datetime Date time string.
+		 * @return bool
+		 */
+		public static function is_past_date( $datetime ) {
+
+			$date = new DateTime( $datetime );
+
+			$current = new DateTime( current_time( 'mysql' ) ); // current date.
+
+			if ( $date > $current ) {
+				return false; // date hasn't been passed.
+			}
+
+			return true; // date has been passed.
+		}
+
+
+	}
+}

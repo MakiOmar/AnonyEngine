7796593083093922f4b34a4f92efabf42d0f5f69
unknown <mo7amed.maki@gmail.com>
2022-04-18 00:31:08 +0200

    Comply to WPCS

--- helpme/wp/class-anony-wp-db-help.php
+++ helpme/wp/class-anony-wp-db-help.php
@@ -26,23 +26,49 @@ if ( ! class_exists( 'ANONY_Wp_Db_Help' ) ) {
 	class ANONY_Wp_Db_Help extends ANONY_HELP {
 
 		/**
-		 * Query published posts ids.
+		 * Get results and make sure check cache first.
 		 *
-		 * @param string $post_type Post type slug.
+		 * @param string $prepared_query Mysql query.Must be prepared first.
 		 * @param string $cache_key WP cache key.
-		 * @param int    $limi Query limit
-		 * @return mixed        Query result.
+		 * @param string $output The output of get_results.
+		 * @param string $group Where to group the cache contents. Enables the same key to be used across groups.
+		 * @param int    $expiry When to expire the cache contents, in seconds. Default 0 (no expiration).
+		 * @return array|object|null Query result.
 		 */
-		public static function get_posts_ids( $post_type, $cache_key, $limit = 20 ) {
+		public static function get_result( $prepared_query, $cache_key, $output = OBJECT, $group = '', $expiry = 0 ) {
 
 			global $wpdb;
 
 			$results = wp_cache_get( $cache_key );
 
 			if ( false === $results ) {
-				$results = $wpdb->get_results(
-					$wpdb->prepare(
-						"SELECT 
+
+				// phpcs:disable
+				$results = $wpdb->get_results( $prepared_query, $output );
+				// phpcs:enable
+
+				wp_cache_set( $cache_key, $results, $group, $expiry );
+
+			}
+
+			return $results;
+		}
+
+		/**
+		 * Query published posts ids.
+		 *
+		 * @param string $post_type Post type slug.
+		 * @param string $cache_key WP cache key.
+		 * @param int    $limit Query limit.
+		 * @return mixed        Query result.
+		 */
+		public static function get_posts_ids( $post_type, $cache_key, $limit = 20 ) {
+
+			global $wpdb;
+
+			$results = self::get_result(
+				$wpdb->prepare(
+					"SELECT 
 							ID 
 						FROM 
 							$wpdb->posts 
@@ -52,15 +78,15 @@ if ( ! class_exists( 'ANONY_Wp_Db_Help' ) ) {
 						    post_status = %s 
 						LIMIT 
 							%d",
-						$post_type,
-						'publish',
-						$limit
-					),
-					ARRAY_A
-				);
-
-				wp_cache_set( $cache_key, $results, '', 3600 );
-			}
+					$post_type,
+					'publish',
+					$limit
+				),
+				$cache_key,
+				ARRAY_A,
+				'',
+				3600
+			);
 
 			ANONY_Wp_Debug_Help::printDbErrors( $results );
 
@@ -70,9 +96,9 @@ if ( ! class_exists( 'ANONY_Wp_Db_Help' ) ) {
 		/**
 		 * Delete term's posts with all connected meta
 		 *
-		 * @param string $post_type
-		 * @param int    $term_id
-		 * @param int    $limit
+		 * @param string $post_type Post type.
+		 * @param int    $term_id Term's ID.
+		 * @param int    $limit Query limit.
 		 * @return Mixed
 		 */
 		public static function delete_term_posts_completely( $post_type, $term_id, $limit = 20 ) {
@@ -81,20 +107,19 @@ if ( ! class_exists( 'ANONY_Wp_Db_Help' ) ) {
 
 			$cache_key = "delete_term_posts_completely_{$post_type}";
 
-			$posts_ids = self::get_posts_ids( $post_type, $cache_key, $limit = 20 );
+			$posts_ids = self::get_posts_ids( $post_type, $cache_key, $limit );
 
 			if ( empty( $posts_ids ) ) {
 				ANONY_Wp_Debug_Help::error_log( "delete_term_posts_completely_{$post_type}: No posts to delete" );
 				return;
 			}
 
-			$query = implode( ',', $posts_ids );
+			$query_ids = implode( ',', $posts_ids );
 
-			$result = $wpdb->query(
-				$wpdb->prepare(
-					"
+			// phpcs:disable
+			$result = $wpdb->query( $wpdb->prepare( "
 			    	DELETE 
-			    		*
+			    		posts,pm,pt
 			        FROM 
 			        	$wpdb->posts posts
 			        LEFT JOIN 
@@ -102,19 +127,16 @@ if ( ! class_exists( 'ANONY_Wp_Db_Help' ) ) {
 			        ON 
 			        	pm.post_id 
 			        IN 
-			        	('{$query}')
+			        	('{$query_ids}') 
 			        LEFT JOIN 
 			        	$wpdb->term_relationships pt 
 			        ON 
-			        	( pt.object_id IN  ('{$query}') AND pt.term_taxonomy_id = %d )
+			        	( pt.object_id IN  ('{$query_ids}') AND pt.term_taxonomy_id = %d )
 			        WHERE 
 			        	posts.ID 
-			        IN ('{$query}')
-			        ",
-					$term_id,
-				)
-			);
-
+			        IN ('{$query_ids}')
+			        ", $term_id ) );
+			// phpcs:enable.
 			return $result;
 		}
 	}

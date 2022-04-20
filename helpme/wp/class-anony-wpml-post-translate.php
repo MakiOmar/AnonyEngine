467715214d28c4e32df31381da6f46ad70311f2f
unknown <mo7amed.maki@gmail.com>
2022-04-21 01:17:05 +0200

    Comply to WPCS

--- helpme/wp/class-anony-wpml-post-translate.php
+++ helpme/wp/class-anony-wpml-post-translate.php
@@ -1,14 +1,30 @@
 <?php
 /**
- * WPML Post helpers class
+ * WPML Post helpers.
  *
- * @package Anonymous theme
- * @author Makiomar
- * @link http://makiomar.com
+ * PHP version 7.3 Or Later.
+ *
+ * @package  AnonyEngine.
+ * @author   Makiomar <info@makior.com>.
+ * @license  https:// makiomar.com AnonyEngine Licence.
+ * @link     https:// makiomar.com/anonyengine
  */
 
-if ( ! class_exists( 'ANONY_WPML_POST_TRANSLATE' ) ) {
-	class ANONY_WPML_POST_TRANSLATE extends ANONY_WPML_HELP {
+defined( 'ABSPATH' ) || die(); // Exit if accessed direct.
+
+if ( ! class_exists( 'ANONY_Wpml_Post_Translate' ) ) {
+
+	/**
+	 * WPML Post helpers class.
+	 *
+	 * PHP version 7.3 Or Later.
+	 *
+	 * @package  AnonyEngine.
+	 * @author   Makiomar <info@makior.com>.
+	 * @license  https:// makiomar.com AnonyEngine Licence.
+	 * @link     https:// makiomar.com/anonyengine
+	 */
+	class ANONY_Wpml_Post_Translate extends ANONY_WPML_HELP {
 
 
 

da3376ca782071acd366c14f681688e708c78599
unknown <mo7amed.maki@gmail.com>
2022-05-05 01:11:37 +0200

    Require options file

--- config.php
+++ config.php
@@ -40,6 +40,7 @@ require_once wp_normalize_path( ANOE_DIR . 'metaboxes/metaboxes.php' );
 require_once wp_normalize_path( ANOE_DIR . 'helpme/helpme.php' );
 require_once wp_normalize_path( ANOE_DIR . 'input-fields/index.php' );
 require_once wp_normalize_path( ANOE_DIR . 'options/options.php' );
+require_once wp_normalize_path( ANOE_DIR . 'anonyengine-options.php' );
 // require_ once( wp_normalize_path( ANOE_DIR . 'utilities/utilities.php') );.
 
 

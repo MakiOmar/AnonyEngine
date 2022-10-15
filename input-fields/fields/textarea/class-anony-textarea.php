383caecc2dd752ec71213ab6df3e4bb9c5f6eb63
unknown <mo7amed.maki@gmail.com>
2022-10-15 11:11:27 +0200

    Add missing markup to textarea input field

--- input-fields/fields/textarea/class-anony-textarea.php
+++ input-fields/fields/textarea/class-anony-textarea.php
@@ -40,6 +40,7 @@ class ANONY_Textarea {
 
 		$rows       = isset( $this->parent->field['rows'] ) ? $this->parent->field['rows'] : 5;
 		$text_align = isset( $this->parent->field['text-align'] ) ? $this->parent->field['text-align'] : 'initial';
+		$direction = isset( $this->parent->field['direction'] ) ? $this->parent->field['direction'] : 'initial';
 
 		if ( $this->parent->as_template ) {
 			$html  = sprintf(
@@ -48,8 +49,9 @@ class ANONY_Textarea {
 				$this->parent->width
 			);
 			$html .= sprintf(
-				'<textarea style="text-align:%1$s" class="%2$s anony-row" rows="' . $rows . '" cols="' . $cols . '" name="%3$s" %4$s %5$s %5$s></textarea>',
+				'<textarea style="text-align:%1$s;direction:%2$s" class="%3$s anony-row" rows="' . $rows . '" cols="' . $cols . '" name="%4$s" %5$s %6$s %7$s></textarea>',
 				$text_align,
+				$direction,
 				$class,
 				$this->parent->input_name,
 				$readonly,
@@ -65,6 +67,8 @@ class ANONY_Textarea {
 			$this->parent->field['id'],
 			$this->parent->width
 		);
+
+		$html .= '<div>';
 		if ( in_array( $this->parent->context, array( 'meta', 'form' ) ) && isset( $this->parent->field['title'] ) ) {
 			$html .= sprintf(
 				'<label class="anony-label" for="anony_%1$s">%2$s</label>',
@@ -73,9 +77,14 @@ class ANONY_Textarea {
 			);
 		}
 
+		if ( isset( $this->parent->field['note'] ) ) {
+			$html .= '<p class=anony-warning>' . $this->parent->field['note'] . '<p>';
+		}
+
 		$html .= sprintf(
-			'<textarea style="text-align:%1$s" class="%2$s" rows="' . $rows . '" cols="' . $cols . '" id="%3$s" name="%3$s" %4$s %5$s %6$s>%7$s</textarea>',
+			'<textarea style="text-align:%1$s;direction:%2$s" class="%3$s" rows="' . $rows . '" cols="' . $cols . '" id="%4$s" name="%4$s" %5$s %6$s %7$s>%8$s</textarea>',
 			$text_align,
+			$direction,
 			$class,
 			$this->parent->input_name,
 			$readonly,
@@ -84,7 +93,16 @@ class ANONY_Textarea {
 			$this->parent->value
 		);
 
+		$html .= '</div>';
+		
+		if( !empty( $this->parent->field['desc'] ) )
+		{
+			$html .= sprintf('<p class="input-field-description">%s</p>', $this->parent->field['desc']);
+		}
+		
 		$html .= '</fieldset>';
+
+
 
 		return $html;
 

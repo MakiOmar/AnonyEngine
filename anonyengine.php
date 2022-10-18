1de01e1681d94e4bec6909d68d3edaf530bbda76
unknown <mo7amed.maki@gmail.com>
2022-10-18 19:01:28 +0200

    Change version

--- anonyengine.php
+++ anonyengine.php
@@ -3,7 +3,7 @@
  * Plugin Name: AnonyEngine
  * Plugin URI: https://makiomar.com
  * Description: With AnonyEngine you can add any kind of metaboxes and options pages and forms easily and supper fast
- * Version: 1.0.01
+ * Version: 1.0.012
  *
  * @package  AnonyEngine
  * Author: Mohammad Omar

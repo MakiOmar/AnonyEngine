fdd54ed0b6644b4cd6a62789a74f7b8bfcb67d12
unknown <mo7amed.maki@gmail.com>
2022-11-26 22:45:57 +0200

    Add new field to handle custom callbacks

---
+++ input-fields/fields/callback/class-anony-callback.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Callback field class
+ *
+ * @package Anonymous theme
+ * @author Makiomar
+ * @link http://makiomar.com
+ */
+class ANONY_Callback {
+
+	/**
+	 * Color field Constructor.
+	 *
+	 * @param object $parent Field parent object
+	 */
+	public function __construct( $parent = null ) {
+		if ( ! is_object( $parent ) ) {
+			return;
+		}
+
+		$this->parent = $parent;
+
+		$this->parent->value = esc_attr( $this->parent->value );
+	}
+
+	/**
+	 * Callback field render Function.
+	 *
+	 * @return mixed
+	 */
+	public function render( $meta = false ) {
+		if ( key_exists( 'callback', $this->parent->field ) ) {
+			return call_user_func($this->parent->field['callback'], $this->parent);
+		}
+	}
+
+}
+

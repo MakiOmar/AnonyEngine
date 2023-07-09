8906ab48c02857acf424faf9927d8e2b4d3fb11b
makiomar <mo7amed.maki@gmail.com>
2023-07-09 17:50:17 +0300

    Consider variable products

--- helpme/wp/class-anony-woo-cart-help.php
+++ helpme/wp/class-anony-woo-cart-help.php
@@ -25,28 +25,45 @@ if ( ! class_exists( 'ANONY_Woo_Cart_Help' ) ) {
 	 */
 	class ANONY_Woo_Cart_Help extends ANONY_Woo_Help {
 
-		public static function get_cart_items($user_id, $product_id = false) {
+		public static function get_cart_items($user_id, $_product_id = false) {
 			global $woocommerce;
 			
 			$cart = $woocommerce->cart->get_cart();
 			$items = array();
-			
+			$parent_product_id = 0;
+			$variation_id = 0;
 			foreach ($cart as $item) {
-		
 				$product = $item['data'];
-				if( $product_id && $product_id !== $product->get_id()  ){
-					continue;
-				}
+				$product_id = $product->get_id();
 				$quantity = $item['quantity'];
 				$price = $product->get_price();
+				
+				// Check if product is a variation
+				if ($product->is_type('variation')) {
+					if( $_product_id && $_product_id !== $product->get_parent_id()  ){
+						continue;
+					}
+					$parent_product_id = $product->get_parent_id();
+					$variation_id = $product_id;
+					$product_id = $parent_product_id;
+				} else {
+					if( $_product_id && $_product_id !== $product_id  ){
+						continue;
+					}
+					$parent_product_id = 0;
+					$variation_id = 0;
+				}
+				
 				$items[] = array(
 					'user_id' => $user_id,
-					'product_id' => $product->get_id(),
+					'product_id' => $product_id,
+					'parent_product_id' => $parent_product_id,
+					'variation_id' => $variation_id,
 					'quantity' => $quantity,
 					'price' => $price
 				);
 			}
-			error_log(print_r($items,true));
+			error_log(print_r($items, true));
 			return $items;
 		}
 	}

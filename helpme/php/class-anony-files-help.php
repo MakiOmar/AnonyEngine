8f32ced955b37f7624b7979ff16ae820ef7ad38a
makiomar <mo7amed.maki@gmail.com>
2023-10-21 20:42:37 +0300

    New helper class

---
+++ helpme/php/class-anony-files-help.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * PHP Files helpers.
+ *
+ * PHP version 7.3 Or Later.
+ *
+ * @package  AnonyEngine
+ * @author   Makiomar <info@makiomar.com>
+ * @license  https:// makiomar.com AnonyEngine Licence.
+ * @link     https:// makiomar.com/anonyengine
+ */
+
+defined( 'ABSPATH' ) || die();
+
+if ( ! class_exists( 'ANONY_FILES_HELP' ) ) {
+
+	/**
+	 * PHP Files helpers class.
+	 *
+	 * PHP version 7.3 Or Later.
+	 *
+	 * @package  AnonyEngine
+	 * @author   Makiomar <info@makiomar.com>
+	 * @license  https:// makiomar.com AnonyEngine Licence.
+	 * @link     https:// makiomar.com/anonyengine
+	 */
+	class ANONY_FILES_HELP extends ANONY_HELP {
+		
+	}
+}

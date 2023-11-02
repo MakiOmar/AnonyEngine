e7e62e33fba597999dc6e11bd053af5e830cb145
makiomar <mo7amed.maki@gmail.com>
2023-11-03 00:50:47 +0200

    Add radio sample

---
+++ input-fields/fields/radio/radio-options.sample.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Radio option sample.
+ *
+ * @package Fields inputs
+ */
+
+//phpcs:disable
+$options = array(
+    'featured-cat'  => array(
+        'title' => esc_html__('Featured category', ANONY_TEXTDOM),
+        'class' => 'slider'
+    ),
+
+    'featured-post' => array(
+        'title' => esc_html__('Featured posts', ANONY_TEXTDOM),
+        'class' => 'slider'
+    ),
+);

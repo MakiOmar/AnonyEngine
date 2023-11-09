d266b12efa73ce6b4b46f5ca5781e9a635058c7a
makiomar <mo7amed.maki@gmail.com>
2023-11-09 23:18:48 +0200

    WPCS

--- metaboxes/classes/class-anony-term-metabox.php
+++ metaboxes/classes/class-anony-term-metabox.php
@@ -1,10 +1,41 @@
 <?php
-if ( ! class_exists( 'ANONY_Term_Metabox' ) ) {
+/**
+ * Term metaboxes class file.
+ *
+ * PHP version 7.3 Or Later.
+ *
+ * @package  AnonyEngine.
+ * @author   Makiomar <info@makiomar.com>.
+ * @license  https:// makiomar.com AnonyEngine Licence.
+ * @link     https:// makiomar.com/anonyengine.
+ */
 
+defined( 'ABSPATH' ) || die(); // Exit if accessed direct.
+
+if ( ! class_exists( 'ANONY_Term_Metabox' ) ) {
+	/**
+	 * Term metaboxes class file.
+	 *
+	 * PHP version 7.3 Or Later.
+	 *
+	 * @package  AnonyEngine.
+	 * @author   Makiomar <info@makiomar.com>.
+	 * @license  https:// makiomar.com AnonyEngine Licence.
+	 * @link     https:// makiomar.com/anonyengine.
+	 */
 	class ANONY_Term_Metabox extends ANONY_Meta_Box {
+		/**
+		 * Taxonomy slug.
+		 *
+		 * @var string
+		 */
+
+		public $taxonomy;
 
 		/**
 		 * Constructor
+		 *
+		 * @param array $meta_box Metabox arguments.
 		 */
 		public function __construct( $meta_box = array() ) {
 
@@ -14,18 +45,20 @@ if ( ! class_exists( 'ANONY_Term_Metabox' ) ) {
 
 			$this->metabox = $meta_box;
 
-			// Set metabox's data
+			// Set metabox's data.
 			$this->set_metabox_data( $this->metabox );
 
-			add_action( 'init', array( $this, 'registerTermMetaKey' ) );
-			add_action( $this->taxonomy . '_add_form_fields', array( $this, 'addFormFields' ) );
+			add_action( 'init', array( $this, 'register_term_meta_key' ) );
+			add_action( $this->taxonomy . '_add_form_fields', array( $this, 'add_form_fields' ) );
 			add_action( $this->taxonomy . '_edit_form_fields', array( $this, 'editFormFields' ) );
-			add_action( 'edited_' . $this->taxonomy, array( $this, 'saveTermMeta' ) );
-			add_action( 'create_' . $this->taxonomy, array( $this, 'saveTermMeta' ) );
+			add_action( 'edited_' . $this->taxonomy, array( $this, 'save_term_meta' ) );
+			add_action( 'create_' . $this->taxonomy, array( $this, 'save_term_meta' ) );
 		}
 
 		/**
-		 * Set metabox data
+		 * Set metabox data.
+		 *
+		 * @param array $metabox Metabox arguments.
 		 */
 		public function set_metabox_data( $metabox ) {
 			$this->id       = $metabox['id'];
@@ -36,24 +69,29 @@ if ( ! class_exists( 'ANONY_Term_Metabox' ) ) {
 
 		/**
 		 * Save term meta value
+		 *
+		 * @param int $term_id Term's ID.
 		 */
-		public function saveTermMeta( $term_id ) {
-			if ( ! isset( $_POST[ $this->id . '_nonce' ] ) || ! wp_verify_nonce( $_POST[ $this->id . '_nonce' ], $this->id . '_action' ) ) {
+		public function save_term_meta( $term_id ) {
+			if ( isset( $_POST ) ) {
+				$submitted_data = wp_unslash( $_POST );
+			}
+			if ( ! isset( $submitted_data[ $this->id . '_nonce' ] ) || ! wp_verify_nonce( $submitted_data[ $this->id . '_nonce' ], $this->id . '_action' ) ) {
 				return;
 			}
 
 			$old_value = get_term_meta( $term_id, $this->id, true );
 
-			if ( $old_value === $_POST[ $this->id ] ) {
+			if ( $old_value === $submitted_data[ $this->id ] ) {
 				return;
 			}
 
-			$term_value[ $this->id ] = $_POST[ $this->id ];
+			$term_value[ $this->id ] = $submitted_data[ $this->id ];
 
-			if ( $old_value && '' === $_POST[ $this->id ] ) {
+			if ( $old_value && '' === $submitted_data[ $this->id ] ) {
 				delete_term_meta( $term_id, $this->id );
 
-			} elseif ( $old_value !== $_POST[ $this->id ] ) {
+			} elseif ( $old_value !== $submitted_data[ $this->id ] ) {
 				update_term_meta( $term_id, $this->id, $term_value );
 			}
 		}
@@ -61,12 +99,14 @@ if ( ! class_exists( 'ANONY_Term_Metabox' ) ) {
 		/**
 		 * Register term meta key
 		 */
-		public function registerTermMetaKey() {
+		public function register_term_meta_key() {
 			register_meta( 'term', $this->id, array( $this, 'validateTermMeta' ) );
 		}
 
 		/**
-		 * validate metabox
+		 * Validate metabox
+		 *
+		 * @param mixed $value term value.
 		 */
 		public function validateTermMeta( $value ) {
 
@@ -76,29 +116,33 @@ if ( ! class_exists( 'ANONY_Term_Metabox' ) ) {
 		/**
 		 * Add metabox on add term page
 		 */
-		public function addFormFields() {
-			if ( $GLOBALS['pagenow'] !== 'edit-tags.php' ) {
+		public function add_form_fields() {
+			if ( 'edit-tags.php' !== $GLOBALS['pagenow'] ) {
 				return;
 			}
 			echo '<div class="form-field anony-term-meta-wrap">';
-			echo $this->meta_fields_callback();
+			$this->meta_fields_callback();
 			echo '</div>';
 		}
 
 		/**
 		 * Add metabox on edit term page
+		 *
+		 * @param object $term Term's ID.
 		 */
 		public function editFormFields( $term ) {
-			if ( $GLOBALS['pagenow'] !== 'term.php' ) {
+			if ( 'term.php' !== $GLOBALS['pagenow'] ) {
 				return;
 			}
 			echo '<div class="form-field anony-term-meta-wrap">';
-			echo $this->meta_fields_callback( $term->term_id );
+			$this->meta_fields_callback( $term->term_id );
 			echo '</div>';
 		}
 
 		/**
 		 * Render metabox' fields.
+		 *
+		 * @param mixed $object_id Object ID or null.
 		 */
 		public function meta_fields_callback( $object_id = null ) {
 
@@ -109,7 +153,7 @@ if ( ! class_exists( 'ANONY_Term_Metabox' ) ) {
 
 			wp_nonce_field( $this->id . '_action', $this->id . '_nonce', false );
 
-			// Loop through inputs to render
+			// Loop through inputs to render.
 			foreach ( $this->fields as $field ) {
 				if ( ! is_null( $object_id ) ) {
 					$render_field = new ANONY_Input_Field( $field, $this->id, 'term', intval( $object_id ) );
@@ -117,10 +161,11 @@ if ( ! class_exists( 'ANONY_Term_Metabox' ) ) {
 					$render_field = new ANONY_Input_Field( $field, $this->id, 'term' );
 				}
 
+				//phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
 				echo $render_field->field_init();
+				//phpcs:enable.
 
 				$this->enqueue_field_scripts( $field );
-
 			}
 		}
 	}

90264d97464d4634bf300b4e8efe7c2588a07db5
makiomar <mo7amed.maki@gmail.com>
2024-02-26 11:43:18 +0200

    Add helper that requires all php files

--- helpme/php/class-anony-files-help.php
+++ helpme/php/class-anony-files-help.php
@@ -25,6 +25,20 @@ if ( ! class_exists( 'ANONY_FILES_HELP' ) ) {
 	 * @link     https:// makiomar.com/anonyengine
 	 */
 	class ANONY_FILES_HELP extends ANONY_HELP {
-
+		/**
+		 * Helper that require all files in a folder/subfolders once.
+		 *
+		 * @param string $dir Directory path.
+		 * @return void
+		 */
+		public static function require_all_files( $dir ) {
+			foreach ( glob( "$dir/*" ) as $path ) {
+				if ( preg_match( '/\.php$/', $path ) ) {
+					require_once $path; // It's a PHP file, so just require it.
+				} elseif ( is_dir( $path ) ) {
+					atrn_require_all_files( $path ); // It's a subdir, so call the same function for this subdir.
+				}
+			}
+		}
 	}
 }

55f7871c64809fbd93ba14bfa23537583778b166
makiomar <mo7amed.maki@gmail.com>
2024-05-02 09:56:37 +0300

    Separate lazyload from image dimensions

--- helpme/php/class-anony-images-help.php
+++ helpme/php/class-anony-images-help.php
@@ -101,31 +101,27 @@ if ( ! class_exists( 'ANONY_IMAGES_HELP' ) ) {
 
 					$img_size = false;
 				}
+				$replaced_img = $imgs[0][ $i ];
+				if ( $lazyload && false === strpos( $replaced_img, 'no-lazyload' ) ) {
+					// Use Defer.js to lazyload.
+					// https://github.com/shinsenter/defer.js/#Defer.lazy.
+					if ( false === strpos( $imgs[0][ $i ], 'data-src' ) ) {
+						$replaced_img = preg_replace( '/<img([^>]*)src=("|\')([^"\']*)(\2)([^>]*)>/', '<img$1data-src=$2$3$4$5 src="data:image/svg+xml,%3Csvg%20xmlns=\'http://www.w3.org/2000/svg\'%20viewBox=\'0%200%20225%20225\'%3E%3C/svg%3E">', $imgs[0][ $i ] );
+					}
 
+					if ( false === strpos( $replaced_img, 'data-srcset' ) ) {
+						$replaced_img = preg_replace( '/<img([^>]*)srcset=("|\')([^"\']*)(\2)([^>]*)>/', '<img$1data-srcset=$2$3$4$5>', $replaced_img );
+					}
+
+					$replaced_img = str_replace( '<img ', '<img loading="lazy" ', $replaced_img );
+				}
+
+				if ( $lazyload && false !== strpos( $replaced_img, 'no-lazyload' ) && false === strpos( $replaced_img, 'loading="lazy"' ) ) {
+					// Disable wp lazyload.
+					$replaced_img = str_replace( ' loading="lazy"', '', $replaced_img );
+
+				}
 				if ( false !== $img_size ) {
-					$replaced_img = $imgs[0][ $i ];
-
-					if ( $lazyload && false === strpos( $replaced_img, 'no-lazyload' ) ) {
-						// Use Defer.js to lazyload.
-						// https://github.com/shinsenter/defer.js/#Defer.lazy.
-						if ( false === strpos( $imgs[0][ $i ], 'data-src' ) ) {
-							$replaced_img = preg_replace( '/<img([^>]*)src=("|\')([^"\']*)(\2)([^>]*)>/', '<img$1data-src=$2$3$4$5 src="data:image/svg+xml,%3Csvg%20xmlns=\'http://www.w3.org/2000/svg\'%20viewBox=\'0%200%20225%20225\'%3E%3C/svg%3E">', $imgs[0][ $i ] );
-						}
-
-						if ( false === strpos( $replaced_img, 'data-srcset' ) ) {
-							$replaced_img = preg_replace( '/<img([^>]*)srcset=("|\')([^"\']*)(\2)([^>]*)>/', '<img$1data-srcset=$2$3$4$5>', $replaced_img );
-						}
-
-						$replaced_img = str_replace( '<img ', '<img loading="lazy" ', $replaced_img );
-
-					}
-
-					if ( $lazyload && false !== strpos( $replaced_img, 'no-lazyload' ) ) {
-						// Disable wp lazyload.
-						$replaced_img = str_replace( ' loading="lazy"', '', $replaced_img );
-
-					}
-
 					$dimension_attributes = 'width="' . $img_size[0] . 'px" height="' . $img_size[1] . 'px"';
 					if ( empty( $img_size[3] ) ) {
 						$img_size[3] = $dimension_attributes;
@@ -157,6 +153,7 @@ if ( ! class_exists( 'ANONY_IMAGES_HELP' ) ) {
 					if ( false !== strpos( $replaced_img, 'no-lazyload' ) ) {
 						$replaced_img = str_replace( ' decoding="async"', '', $replaced_img );
 					}
+
 					$content = str_replace( $img, $replaced_img, $content );
 				}
 			}
